df9f929e26c276f978bc2a11bce5c3c531a377fd
Mahi <maheshmorde2511@gmail.com>
2024-04-05 09:35:22 +0530

    dockerised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './mycomponent/catalogue/catalogue.component';
+import { CartComponent } from './mycomponent/cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CheckoutPageComponent } from './mycomponent/checkout-page/checkout-page.component';
+import { NavbarComponent } from './mycomponent/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogueComponent,
+    CartComponent,
+    CheckoutPageComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
